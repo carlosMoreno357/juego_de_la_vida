ac9db41ac1659f4f777e619b7cf38d9fc2d3e091
Carlos Ignacio Moreno Salazar <carlos.moreno.salazar@gmail.com>
2019-11-26 00:39:39 -0600

    Variables globales
    Se establecen las variables globales para generar el tablero

--- juego.js
+++ juego.js
@@ -1,1 +1,26 @@
 console.log("----- Juego de la vida Inicializado -----");
+
+!function(){
+
+    var btnGenerar = document.querySelector('#generar');
+    var btnComenzar = document.querySelector('#comenzar');
+    var btnParar = document.querySelector('#parar');
+    var btnLimpiar = document.querySelector('#limpiar');
+
+    var estado = {
+        activo: false,
+        celulas_vivas: {
+            anteriores: [],
+            actuales: []
+        }
+    };
+
+    var tam_celula = 5;
+    
+    var tablero = document.querySelector('#tablero');
+    var tableroContexto = tablero.getContext('2d');
+
+    tablero.style.backgroundColor = '#000';
+    tableroContexto.fillStyle = '#fff';
+    
+}();
